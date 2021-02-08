ac84b623326a4c5cc296da7be43624ca587ccb50
uidq1343 <xuchun.ni@desay-svautomotive.com>
2021-02-08 18:02:07 +0800

    h2h detail

--- trainee/pages/h2h/detail.js
+++ trainee/pages/h2h/detail.js
@@ -8,7 +8,7 @@ Page({
   data: {
     statusBarHeight: getApp().globalData.statusBarHeight,
     totalBarHeight: getApp().globalData.totalBarHeight,
-    visible: false,
+    visible1: false,
     fruit: [{
       id: 1,
       name: '没参加比赛',
@@ -17,19 +17,6 @@ Page({
       name: '比分记错了'
   }],
   current: '没参加比赛',
-  tags:[{ name :"磨神",
-  checked : false},{ name :"进攻凶猛",
-  checked : false},{ name :"发球大炮",
-  checked : false},{ name :"跑不死",
-  checked : false},{ name :"暴力正手",
-  checked : false},
-  { name :"魔鬼切削",
-  checked : false},{ name :"全场进攻",
-  checked : false},{ name :"变化多端",
-  checked : false},{ name :"发球上网",
-  checked : false},{ name :"底线AK47",
-  checked : false},],
-  currentTarget:[],
     actions: [{
         name: '申诉',
         color: '#fff',
@@ -47,54 +34,41 @@ Page({
       }
     ],
     slideButtons: [{
-      text: '范大将军 ',
-      src: '范大将军', // icon的路径,
-      time: '黄金 段位',
-      result: '第1',
+      text: 'jerry     vs      范大将军 ',
+      src: '6  : 7', // icon的路径,
+      time: '2020-12-21',
+      result: '失败',
       score: '-30',
       toggle: false
     }, {
-      text: 'jerry',
-      src: 'jerry', // icon的路径,
-      time: '黄金段位',
-      result: '第2',
-      score: '-30',
-      toggle: false
-    },{
-      text: '小鱼儿 ',
-      src: '小鱼儿', // icon的路径,
-      time: '黄金 段位',
-      result: '第1',
+      text: 'jerry     vs      范大将军 ',
+      src: '6  : 7', // icon的路径,
+      time: '2020-12-21',
+      result: '失败',
       score: '-30',
       toggle: false
     }, {
-      text: 'jerry',
-      src: 'jerry', // icon的路径,
-      time: '黄金段位',
-      result: '第2',
-      score: '-30',
-      toggle: false
-    },{
-      text: '范大将军 ',
-      src: '范大将军', // icon的路径,
-      time: '黄金 段位',
-      result: '第1',
+      text: 'jerry     vs      范大将军 ',
+      src: '6  : 7', // icon的路径,
+      time: '2020-12-21',
+      result: '失败',
       score: '-30',
       toggle: false
     }, {
-      text: 'jerry',
-      src: 'jerry', // icon的路径,
-      time: '黄金段位',
-      result: '第2',
+      text: 'jerry     vs      范大将军 ',
+      src: '6  : 7', // icon的路径,
+      time: '2020-12-21',
+      result: '失败',
+      score: '-30',
+      toggle: false
+    }, {
+      text: 'jerry     vs      范大将军 ',
+      src: '6  : 7', // icon的路径,
+      time: '2020-12-21',
+      result: '失败',
       score: '-30',
       toggle: false
     }],
-  },
-  onChange(event){
-    const detail = event.detail;
-        this.setData({
-            ['tags['+event.detail.name+'].checked'] : detail.checked
-        })
   },
   handleFruitChange({ detail = {} }) {
     this.setData({
@@ -102,7 +76,7 @@ Page({
     });
 },
   handleClose1(){
-    this.setData({visible:false})
+    this.setData({visible1:false})
   },
   handleClickItem2(e) {
     console.log(e.detail, this.data.slideButtons[e.detail.dataIndex].toggle)
@@ -116,13 +90,6 @@ Page({
       this.setData({visible1:true})
     }
 
-  },
-  handleTapped(e){
-    // this.setData({visible:true})
-    console.log(e.detail)
-    wx.navigateTo({
-      url: './detail',
-    })
   },
   /**
    * 生命周期函数--监听页面加载
@@ -142,7 +109,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-console.log(app.globalData)
+
   },
 
   /**
