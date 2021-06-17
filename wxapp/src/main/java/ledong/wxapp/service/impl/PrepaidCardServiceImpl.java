bd8ffb90b556977fbbaa61edf3439fb7f1ef02b8
uidq1343 <xuchun.ni@desay-svautomotive.com>
2021-06-17 15:00:30 +0800

    update member prepaid card

--- wxapp/src/main/java/ledong/wxapp/service/impl/PrepaidCardServiceImpl.java
+++ wxapp/src/main/java/ledong/wxapp/service/impl/PrepaidCardServiceImpl.java
@@ -64,76 +64,80 @@ public class PrepaidCardServiceImpl implements IPrepaidCardService {
     @Autowired
     private IUserService userService;
 
-
     @Override
     public String addCard(String cardName, String... members) {
         LdPrePaidCardVo card = new LdPrePaidCardVo();
         card.setTitle(cardName);
-        if(members!=null){
+        if (members != null) {
             card.setMember(members);
         }
         String createTime = DateUtil.getCurrentDate(DateUtil.FORMAT_DATE_TIME);
         card.setCreateTime(createTime);
-        logger.info(card.toString());
-        String userId = SearchApi.insertDocument(DataSetConstant.LD_PREPAID_CARD_INFORMATION, JSON.toJSONString(card),
+
+        String cardId = SearchApi.insertDocument(DataSetConstant.LD_PREPAID_CARD_INFORMATION, JSON.toJSONString(card),
                 cardName);
-        logger.info(userId);
-        return userId;
+        for (String openId : members) {
+            HashMap<String, Object> vo = userService.getLDUserInfo(openId);
+            vo.put(UserVo.PREPAIDCARD, cardId);
+            SearchApi.updateDocument(DataSetConstant.LD_USER_INFORMATION, JSON.toJSONString(vo), openId);
+        }
+        return cardId;
     }
 
     @Override
     public String assignMember(String cardName, String openId) {
-        HashMap<String, Object>  card=  SearchApi.searchById(DataSetConstant.LD_PREPAID_CARD_INFORMATION,cardName);
-        if(card==null){
+        HashMap<String, Object> card = SearchApi.searchById(DataSetConstant.LD_PREPAID_CARD_INFORMATION, cardName);
+        if (card == null) {
             return null;
-        }else{
-            ArrayList<String> member= ( ArrayList<String>) card.get(LdPrePaidCardVo.MEMBER);
-//          ArrayList<String> temp=new ArrayList<>();
-//            Arrays.stream(member).map((id)->{
-//                temp.add(id);
-//                return null;
-//            });
-            if(member.indexOf(openId)<0){
+        } else {
+            ArrayList<String> member = (ArrayList<String>) card.get(LdPrePaidCardVo.MEMBER);
+
+            if (member.indexOf(openId) < 0) {
+                HashMap<String, Object> vo = userService.getLDUserInfo(openId);
+                vo.put(UserVo.PREPAIDCARD, card.get(LdPrePaidCardVo.TITLE));
+                SearchApi.updateDocument(DataSetConstant.LD_USER_INFORMATION, JSON.toJSONString(vo), openId);
                 member.add(openId);
-                String[] newMember =new  String[member.size()];
-                card.put(LdPrePaidCardVo.MEMBER,member.toArray(newMember));
-                return   SearchApi.updateDocument(DataSetConstant.LD_PREPAID_CARD_INFORMATION,JSON.toJSONString(card), cardName);
-            }else{
+                String[] newMember = new String[member.size()];
+                card.put(LdPrePaidCardVo.MEMBER, member.toArray(newMember));
+                return SearchApi.updateDocument(DataSetConstant.LD_PREPAID_CARD_INFORMATION, JSON.toJSONString(card),
+                        cardName);
+            } else {
                 return cardName;
             }
 
         }
     }
 
-
-
     @Override
-    public String settleAccount(String courseId,String startTime, Double spendTime, HashMap<String, Integer> membersObj) {
-        //log each spend
-        ArrayList<String> members= new ArrayList<>();
-        HashMap<String,Integer> cardCharge=new HashMap<>();
-        for(String m : membersObj.keySet()){
+    public String settleAccount(String courseId, String startTime, Double spendTime,
+            HashMap<String, Integer> membersObj) {
+        // log each spend
+        ArrayList<String> members = new ArrayList<>();
+        HashMap<String, Integer> cardCharge = new HashMap<>();
+        for (String m : membersObj.keySet()) {
             members.add(m);
-            HashMap<String, Object> memberVo =userService.getLDUserInfo(m);
-            String cardId= (String) memberVo.get(UserVo.PREPAIDCARD);
-            if(TextUtils.isEmpty(cardId)){
+            HashMap<String, Object> memberVo = userService.getLDUserInfo(m);
+            String cardId = (String) memberVo.get(UserVo.PREPAIDCARD);
+            if (TextUtils.isEmpty(cardId)) {
                 continue;
             }
-            int temp = cardCharge.get(cardId)==null?0: cardCharge.get(cardId);
-            cardCharge.put(cardId,temp+ membersObj.get(m));
-            LdSpendingVo spendVo=new LdSpendingVo();
+            int temp = cardCharge.get(cardId) == null ? 0 : cardCharge.get(cardId);
+            cardCharge.put(cardId, temp + membersObj.get(m));
+            LdSpendingVo spendVo = new LdSpendingVo();
             spendVo.setOpenId(m);
             spendVo.setTime(startTime);
             spendVo.setSpend(spendTime);
             spendVo.setCharge(membersObj.get(m));
             spendVo.setCourse(cardId);
-            SearchApi.appendFieldValueById(DataSetConstant.LD_PREPAID_CARD_INFORMATION,LdPrePaidCardVo.SPENDING,spendVo, cardId);
+            SearchApi.appendFieldValueById(DataSetConstant.LD_PREPAID_CARD_INFORMATION, LdPrePaidCardVo.SPENDING,
+                    spendVo, cardId);
         }
-        //update total spend
-        for(String card : cardCharge.keySet()){
-            HashMap<String, Object>  cardVo=   SearchApi.searchById(DataSetConstant.LD_PREPAID_CARD_INFORMATION,card);
+        // update total spend
+        for (String card : cardCharge.keySet()) {
+            HashMap<String, Object> cardVo = SearchApi.searchById(DataSetConstant.LD_PREPAID_CARD_INFORMATION, card);
             int temp = (int) cardVo.get(LdPrePaidCardVo.BALANCE);
-            SearchApi.updateFieldValueById(DataSetConstant.LD_PREPAID_CARD_INFORMATION,LdPrePaidCardVo.BALANCE,String.valueOf(temp+cardCharge.get(card)),card)   ;
+            SearchApi.updateFieldValueById(DataSetConstant.LD_PREPAID_CARD_INFORMATION, LdPrePaidCardVo.BALANCE,
+                    String.valueOf(temp + cardCharge.get(card)), card);
         }
         return courseId;
     }
